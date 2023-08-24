2df54132f4f932a3918a6d2874c84f2046826a9c
mikewii <mike.sivkov@gmail.com>
2023-08-24 03:32:17 +0300

    rename

--- tests/demangle.cpp
+++ tests/demangle.cpp
@@ -4,16 +4,13 @@
 #include <array>
 
 namespace {
-using type = std::string_view;
-using pair = std::pair<type, type>;
+using string_type = std::string_view;
+using items = std::pair<string_type, string_type>;
 
-constexpr const pair general[] =
+constexpr const items general[] =
 {
-#ifdef __APPLE__
     {"_Znwi", "operator new(int)"},
-#else
     {"__Znwi", "operator new(int)"},
-#endif
     {"_Z4funcPci", "func(char*, int)"},
     {"_Z1fv", "f()"},
     {"_RINvNtC3std3mem8align_ofdE", "std::mem::align_of::<f64>"},
@@ -29,7 +26,7 @@ constexpr const pair general[] =
     {"_ZN7QObject7connectIM9QComboBoxFviEZN15ParseParametersC4EP7QWidgetEUliE4_EENSt9enable_ifIXeqsrN9QtPrivate15FunctionPointerIT0_EE13ArgumentCountLin1EEN11QMetaObject10ConnectionEE4typeEPKNSA_IT_E6ObjectESH_PKS_SB_N2Qt14ConnectionTypeE", "std::enable_if<QtPrivate::FunctionPointer<ParseParameters::ParseParameters(QWidget*)::{lambda(int)#6}>::ArgumentCount==(-1), QMetaObject::Connection>::type QObject::connect<void (QComboBox::*)(int), ParseParameters::ParseParameters(QWidget*)::{lambda(int)#6}>(QtPrivate::FunctionPointer<void (QComboBox::*)(int)>::Object const*, void (QComboBox::*)(int), QObject const*, ParseParameters::ParseParameters(QWidget*)::{lambda(int)#6}, Qt::ConnectionType)"}
 };
 
-constexpr const pair withPostfix[] =
+constexpr const items withPostfix[] =
 {
     {"_ZN7QWidget11setGeometryERK5QRect@Qt_5", "QWidget::setGeometry(QRect const&)"},
     {"_ZdlPv@GLIBCXX_3.4", "operator delete(void*)"},
@@ -38,14 +35,15 @@ constexpr const pair withPostfix[] =
     {"_ZNKSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEE7_M_dataEv@@GLIBCXX_3.4.21", "std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>::_M_data() const"},
 };
 
-constexpr const pair withPrefix[] =
+constexpr const items withPrefix[] =
 {
     {"__odr_asan._ZN5proto15SceneGadgetInfo22kScreenInfoFieldNumberE", "proto::SceneGadgetInfo::kScreenInfoFieldNumber"},
-    {"_GLOBAL__sub_I__ZN6google8protobuf2io15FileInputStreamC2Eii", "google::protobuf::io::FileInputStream::FileInputStream(int, int)"}
+    {"_GLOBAL__sub_I__ZN6google8protobuf2io15FileInputStreamC2Eii", "google::protobuf::io::FileInputStream::FileInputStream(int, int)"},
+    {"_ZN12hb_partial_tI8._anon_0E1mIiJEEEDTcl4sinkcl6sourceIS0_EEspcvT0__EEEOT_DpOS3_", "decltype (sink((source<._anon_0>)())) hb_partial_t<._anon_0>::m<int>(int&&)"}
 };
 
 template <std::size_t N>
-void demangle(const pair (&pairs)[N])
+void demangle(const items (&pairs)[N])
 {
     for (const auto& pair : pairs) {
         const auto& mangled = pair.first;
